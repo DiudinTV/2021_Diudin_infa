9875f7cf40403b1a4efc8f0b6ee4503d6e3a41c6
DiudinTV <diudin.tv@phystech@edu>
2021-10-15 18:24:24 +0300

    Added triangles

--- Lab5/Game.py
+++ Lab5/Game.py
@@ -33,7 +33,8 @@ possible_shape_number_maximum = 0
 shape_list = []
 count = 0
 
-start_ball_speed = 6  # starting speed of shapes
+start_ball_speed = 5  # starting speed of shapes
+start_triangle_speed = 5
 start_square_speed = 3
 start_wall_speed = 10
 ball_speed = start_ball_speed
@@ -43,8 +44,10 @@ wall_speed = start_wall_speed
 start_wall_width = 50
 wall_width = start_wall_width
 square_fracture_acceleration = 5  # clicked big square will fracture, new squares gain extra speed before hitting wall
+triangle_acceleration = 1  # triangles move under vertical force
 number_of_shapes = 8  # don't set more then 20
 level_goal = 50
+loop = 12  # after obtaining this level, game loops and starts from level 0 keeping only points and time
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -53,27 +56,31 @@ GREEN = (0, 255, 0)
 MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 WHITE = (255, 255, 255)
+VIOLET = (101, 0, 170)
+ORANGE = (252, 102, 0)
 BLACK = (0, 0, 0)
 DARK_RED = (100, 0, 0)
 DARK_GREEN = (0, 100, 0)
 DARK_BLUE = (0, 0, 100)
 GREY = (100, 100, 100)
-COLORS = [WHITE, YELLOW, MAGENTA, CYAN, RED, GREEN, BLUE, BLACK, GREY, GREY]
-TEXT_COLORS = [BLACK, BLUE, GREEN, RED, CYAN, MAGENTA, YELLOW, WHITE, BLACK, BLACK]
+COLORS = [WHITE, YELLOW, MAGENTA, CYAN, ORANGE, VIOLET, RED, GREEN, BLUE, BLACK, GREY, GREY]  # .len() must be == loop
+TEXT_COLORS = [BLACK, BLUE, GREEN, RED, VIOLET, ORANGE, CYAN, MAGENTA, YELLOW, WHITE, BLACK, BLACK]
 WALL_COLORS = [DARK_RED, DARK_GREEN, DARK_BLUE, GREY]
 MESSAGE_LIST2 = ["clicking on background subtracts points",
                  "for every " + str(level_goal) + " points gained, level changes",
+                 "predictable but give small amount of points", "are present, no more figures will appear",
                  "every square hit gives equal amount of points", "are present, no more squares will appear",
                  "will result in losing points", "faster and thinner, but subtract the same amount",
                  "from all sources is equal to level number",
                  "gives bonus points for every additional figure",
-                 "gain " + str(10 * level_goal) + " points to loop"]
-MESSAGE_LIST = ["Hit balls to get points", "Smaller balls give more points", "Squares fracture when hit",
-                "If " + str((number_of_shapes + 3) // 4) + " medium or 1 big square", "Hitting moving wall spaces",
+                 "gain " + str(loop * level_goal) + " points to loop"]
+MESSAGE_LIST = ["Hit balls to get points", "Smaller balls give more points", "Triangles bounce off the edges",
+                "If " + str(number_of_shapes) + "+ figures of any kind", "Squares fracture when hit",
+                "If " + str((number_of_shapes + 3) // 4) + "+ medium or 1 big square", "Hitting moving wall spaces",
                 "New walls will be",
                 "Number of subtracted points", "Hitting 2+ figures with 1 click", "No more figures will appear"]
 level_color = WHITE
-active_colors = [COLORS[i] for i in range(8)]
+active_colors = [COLORS[i] for i in range(loop - 2)]
 active_colors.pop(active_colors.index(level_color))
 
 
@@ -90,6 +97,9 @@ def update_screen():
             dr.circle(screen, col, (shape[1], shape[2]), shape[3])
         elif shape[0] == "square":
             dr.rect(screen, col, (shape[1], shape[2], shape[3], shape[3]))
+        elif shape[0] == "triangle":
+            dr.polygon(screen, col, [(shape[1], shape[2] + shape[3]), (shape[1] + shape[3], shape[2] + shape[3]),
+                                     (shape[1] + shape[3] / 2, shape[2])])
         elif shape[0] == "wall":
             dr.rect(screen, col, (shape[1], shape[2], shape[3], 748))
             dr.rect(screen, WHITE, (shape[1] - 1, shape[2] - 1, shape[3], 750), 5)
@@ -151,7 +161,7 @@ def new_ball(speed):
         direction_y *= 1 / abs(direction_y)
     vx = speed * random() * direction_x
     vy = (speed ** 2 - vx ** 2) ** (1 / 2) * direction_y
-    color = active_colors[randint(0, 5)]
+    color = active_colors[randint(0, 7)]
     dr.circle(screen, color, (x, y), r)
     shape_list.append(["ball", x, y, r, color, vx, vy])
     return 1
@@ -179,7 +189,7 @@ def new_square(speed):
         direction_y *= 1 / abs(direction_y)
     vx = speed * random() * direction_x
     vy = (speed ** 2 - vx ** 2) ** (1 / 2) * direction_y
-    color = active_colors[randint(0, 5)]
+    color = active_colors[randint(0, 7)]
     dr.circle(screen, color, (x, y), a)
     shape_list.append(["square", x, y, a, color, vx, vy])
     if a <= 160:
@@ -188,11 +198,36 @@ def new_square(speed):
         return 21
 
 
+def new_triangle(speed):
+    """
+    :param speed: starting triangle velocity
+
+    draws new triangle
+
+    :return: number of created triangles to control potential number of shapes on screen
+    """
+    x = randint(100, 800)
+    y = randint(200, 800)
+    if level == 4:
+        a = 200  # triangle base and height
+    else:
+        a = randint(120, 240)
+    direction_x = random() - 0.5
+    if direction_x != 0:
+        direction_x *= 1 / abs(direction_x)
+    vx = speed * direction_x
+    vy = 0
+    color = active_colors[randint(0, 7)]
+    dr.polygon(screen, color, [(x, y + a), (x + a, y + a), (x + a / 2, y)])
+    shape_list.append(["triangle", x, y, a, color, vx, vy])
+    return 1
+
+
 def click(event_name):
     """
     :param event_name: holds parameters of our mouseclick
 
-    checks whether mouse was clicked in or outside of the ball, adds point if former, subtracts otherwise
+    checks whether mouse was clicked in or outside of the shape, adds point if former, subtracts otherwise
 
     :return: number of level after click
     """
@@ -209,7 +244,6 @@ def click(event_name):
                 possible_shape_number_maximum -= 1
                 count += 6 - (shape[3] + 10) // 20
                 shape_list.pop(shape_list.index(shape))
-                update_screen()
                 fee -= level + 1
 
         elif shape[0] == "square":
@@ -237,7 +271,17 @@ def click(event_name):
                                 shape_list.pop(shape_list.index(shape))
                                 fee -= level + 1
                                 count += 2
-    count = max(count - fee, count - count % (10 * level_goal))
+        elif shape[0] == "triangle":
+            x = event_name.pos[0]
+            y = event_name.pos[1]
+            if y < shape[2] + shape[3]:
+                if y > 2 * (shape[1] - x) + shape[2] + shape[3]:
+                    if y > 2 * (x - shape[1]) + shape[2] - shape[3]:
+                        possible_shape_number_maximum -= 1
+                        fee -= level + 1
+                        count += 3 - (shape[3] + 60) // 120
+                        shape_list.pop(shape_list.index(shape))
+    count = max(count - fee, count - count % (loop * level_goal))
     return score()
 
 
@@ -248,15 +292,15 @@ def score():
     :return: number of current level (-1)
     """
     global level_color, active_colors
-    level_number = (count % (10 * level_goal)) // level_goal
-    MESSAGE_LIST2[8] = "gain " + str(
-        10 * level_goal * ((count + 10 * level_goal) // (10 * level_goal))) + " points to loop"
-    if level_number == 9:
-        level_number = 8
+    level_number = (count % (loop * level_goal)) // level_goal
+    MESSAGE_LIST2[loop - 2] = "gain " + str(
+        loop * level_goal * ((count + (loop - 2) * level_goal) // (loop * level_goal))) + " points to loop"
+    if level_number == loop - 1:
+        level_number = loop - 2
     level_color = COLORS[level_number]
     text_color = TEXT_COLORS[level_number]
-    if level_number != 8:
-        active_colors = [COLORS[u] for u in range(8)]
+    if level_number != loop - 2:
+        active_colors = [COLORS[u] for u in range(loop - 2)]
         active_colors.pop(active_colors.index(level_color))
         active_colors.pop(active_colors.index(text_color))
     update_screen()
@@ -298,9 +342,10 @@ def move_shapes():
     moves shapes according to their velocity, hitting walls results in randomisation of it
     """
     for shape_number in range(len(shape_list)):
+        if shape_list[shape_number][0] == "triangle":
+            shape_list[shape_number][6] += triangle_acceleration
         shape_list[shape_number][1] += shape_list[shape_number][5]
         shape_list[shape_number][2] += shape_list[shape_number][6]
-
         if shape_list[shape_number][0] == "wall":
             shape_velocity = shape_list[shape_number][5]
             hit_velocity = 0
@@ -313,26 +358,42 @@ def move_shapes():
             shape_velocity = square_speed
             hit_velocity = square_speed * (2 * random() - 1)
             sizes = [shape_list[shape_number][3], 0] * 2
+        elif shape_list[shape_number][0] == "triangle":
+            sizes = [shape_list[shape_number][3], 0] * 2
+            shape_velocity = 0
+            hit_velocity = 0
         else:
             shape_velocity = 0
             hit_velocity = 0
             sizes = [0] * 4
 
         if shape_list[shape_number][1] - sizes[1] <= 0:
-            shape_list[shape_number][5] = (shape_velocity ** 2 - hit_velocity ** 2) ** (1 / 2)
-            shape_list[shape_number][6] = hit_velocity  # randomizes velocities after hitting a wall
+            if shape_list[shape_number][0] == "triangle":
+                shape_list[shape_number][5] *= -1
+            else:
+                shape_list[shape_number][5] = (shape_velocity ** 2 - hit_velocity ** 2) ** (1 / 2)
+                shape_list[shape_number][6] = hit_velocity  # randomizes velocities after hitting a wall
             shape_list[shape_number][1] -= shape_list[shape_number][1] - sizes[1]
         elif shape_list[shape_number][1] + sizes[0] >= 1250:
-            shape_list[shape_number][5] = -(shape_velocity ** 2 - hit_velocity ** 2) ** (1 / 2)
-            shape_list[shape_number][6] = hit_velocity
+            if shape_list[shape_number][0] == "triangle":
+                shape_list[shape_number][5] *= -1
+            else:
+                shape_list[shape_number][5] = -(shape_velocity ** 2 - hit_velocity ** 2) ** (1 / 2)
+                shape_list[shape_number][6] = hit_velocity
             shape_list[shape_number][1] -= shape_list[shape_number][1] + sizes[0] - 1250
         if shape_list[shape_number][2] - sizes[3] <= 0:
-            shape_list[shape_number][6] = (shape_velocity ** 2 - hit_velocity ** 2) ** (1 / 2)
-            shape_list[shape_number][5] = hit_velocity
+            if shape_list[shape_number][0] == "triangle":
+                shape_list[shape_number][6] *= -0.5
+            else:
+                shape_list[shape_number][6] = (shape_velocity ** 2 - hit_velocity ** 2) ** (1 / 2)
+                shape_list[shape_number][5] = hit_velocity
             shape_list[shape_number][2] -= shape_list[shape_number][2] - sizes[3]
         elif shape_list[shape_number][2] + sizes[2] >= 750:
-            shape_list[shape_number][6] = -(shape_velocity ** 2 - hit_velocity ** 2) ** (1 / 2)
-            shape_list[shape_number][5] = hit_velocity
+            if shape_list[shape_number][0] == "triangle":
+                shape_list[shape_number][6] *= -1
+            else:
+                shape_list[shape_number][6] = -(shape_velocity ** 2 - hit_velocity ** 2) ** (1 / 2)
+                shape_list[shape_number][5] = hit_velocity
             shape_list[shape_number][2] -= shape_list[shape_number][2] + sizes[2] - 750
         update_screen()
         score()
@@ -354,27 +415,41 @@ while not finished:
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
             level = click(event)
-    if level == 8:
+    if level == loop - 2:
         for i in range(wall_count + 1):
             shape_list.pop(0)
             wall_count -= 1
-    elif 3 <= level < wall_count + 4:
-        while level < wall_count + 4:
+    elif loop - 7 <= level < wall_count + loop - 6:
+        while level < wall_count + loop - 6:
             shape_list.pop(wall_count)
             wall_count -= 1
-    elif level >= 4 and len(shape_list) != 0 and wall_count + 4 < level:
+    elif level >= loop - 6 and len(shape_list) != 0 and wall_count + loop - 6 < level:
         wall_count += 1
-        wall = ["wall", 1, 1, wall_width * (4 - wall_count), WALL_COLORS[wall_count], wall_speed * (wall_count + 1), 0]
+        wall = ["wall", 1, 1, wall_width * (loop - 6 - wall_count), WALL_COLORS[wall_count],
+                wall_speed * (wall_count + 1), 0]
         shape_list.append(shape_list[wall_count])
         shape_list[wall_count] = wall
     elif len(shape_list) - wall_count - 1 < number_of_shapes:
-        if random() >= 0.2 or level <= 1 or possible_shape_number_maximum >= 2 * number_of_shapes - 1:
+        if level <= 1:
             ball_speed = start_ball_speed + level
             possible_shape_number_maximum += new_ball(ball_speed)
+        elif level <= 3 or possible_shape_number_maximum >= 2 * number_of_shapes - 1:
+            if random() >= 0.5:
+                triangle_speed = start_triangle_speed + level
+                possible_shape_number_maximum += new_triangle(triangle_speed)
+            else:
+                ball_speed = start_ball_speed + level
+                possible_shape_number_maximum += new_ball(ball_speed)
         else:
-            square_speed = start_square_speed + level / 2
-            possible_shape_number_maximum += new_square(square_speed)
-
+            if random() >= 0.7:
+                triangle_speed = start_triangle_speed + level
+                possible_shape_number_maximum += new_triangle(triangle_speed)
+            elif random() >= 0.4:
+                square_speed = start_square_speed + level / 2
+                possible_shape_number_maximum += new_square(square_speed)
+            else:
+                ball_speed = start_ball_speed + level
+                possible_shape_number_maximum += new_ball(ball_speed)
     time += 1  # actually number of frames
     move_shapes()
     pygame.display.update()
